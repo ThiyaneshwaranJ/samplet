43826a6f4ffe1bfba32a3bf8ac6bc874bce0bc82
ThiyaneshwaranJ <98819473+ThiyaneshwaranJ@users.noreply.github.com>
2022-02-07 17:32:05 +0530

    Add files via upload

---
+++ maven1/src/test/java/newmaven/maven1/Test1.java
@@ -0,0 +1,12 @@
+package newmaven.maven1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=3;
+		System.out.println("Answer is="+(a+b));
+
+	}
+
+}
